73260fd492ff323112b046e8d613ec3b94e32bab
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-22 11:03:42 +0530

    add cultureInfo class

--- Code Challenge_Day 26 Nested Logic/Program.cs
+++ Code Challenge_Day 26 Nested Logic/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Code_Challenge_Day_26_Nested_Logic
 {
@@ -6,9 +7,29 @@ namespace Code_Challenge_Day_26_Nested_Logic
     {
         static void Main(string[] args)
         {
-            DateTime actualDate = Convert.ToDateTime(Console.ReadLine());
-            DateTime expectedDate = Convert.ToDateTime(Console.ReadLine());
+            CultureInfo cultureInfo = new CultureInfo("en-US");
 
+            DateTime returnedDate = Convert.ToDateTime(Console.ReadLine(),cultureInfo);
+            DateTime expectedDate = Convert.ToDateTime(Console.ReadLine(),cultureInfo);
+
+            if(returnedDate < expectedDate)
+            {
+                Console.WriteLine(0);
+            }
+            else if(returnedDate.Year > expectedDate.Year)
+            {
+                Console.WriteLine(10000);
+            }
+
+            else if(returnedDate.Month >expectedDate.Month)
+            {
+                Console.WriteLine(500 *(returnedDate.Month-expectedDate.Month));
+            }
+
+            else if(returnedDate.Day > expectedDate.Day)
+            {
+                Console.WriteLine(15 *(returnedDate.Day - expectedDate.Day));
+            }
         }
     }
 }
